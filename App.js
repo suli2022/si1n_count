6328d5736f65e6e9f754ddccf2ca90b31096329d
Nagy Janos <nagy@zold.lan>
2023-06-06 16:21:22 +0200

    Kész

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TextInput, 
+  Button
+} from 'react-native';
+import React from 'react';
 
 export default function App() {
+  
+  const [counter, setCounter] = React.useState(0);
+
+  function doit() {
+    let sum = Number(counter) + 1;
+    setCounter(sum)    
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.elso}>Számláló</Text>
+      
+      <Button title='Számol' 
+      onPress={doit}/>
+      
+      <View>
+        <Text style={styles.elso}>
+          {counter}
+        </Text>
+      </View>
+
+      <Button title="Nulla" onPress={ () => setCounter(0)} />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#008',
     alignItems: 'center',
     justifyContent: 'center',
+    
+  },
+  elso: {
+    color: '#fff',
+    fontSize: 30,
+  },
+  input: {
+    backgroundColor: 'lightblue',
+    width: '90%',
+    fontSize: 30,
   },
 });
